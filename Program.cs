063bf5eb758982a0f33fa99ba72bc99bd9e0c129
sklifford <kogutvs@gmail.com>
2016-04-25 22:49:32 +0300

    Added files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿//Показать на экране лесенку из заданого пользователем количества ступеней
+
+using System;
+
+namespace HomeWork1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int32 Num = 0;
+            Int32 Step = 2;
+            Int32 Res = 0;
+            Console.WriteLine("Укажите количество ступеней для прорисовки лестницы");
+            String temp = Console.ReadLine();
+            Num = Int16.Parse(temp);
+            for (Int32 i = 0; i < Num; i++)
+            {
+                Res = Step * i;
+                for (Int32 j = 0; j < Res; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine("***");
+                for (Int32 j = 0; j < Res; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine("  *");
+            }
+            for (Int32 j = 0; j < Res + Step; j++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine("***");
+        }
+    }
+}
